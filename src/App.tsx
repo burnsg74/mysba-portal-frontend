8e7bb6455c6ae2de91587f86999a8d23bd66dbbf
Peter Cofrancesco <pcofrancesco@makpar.com>
2024-01-05 10:57:34 -0500

    merging feature/sidenav-header with current branch to avoid merge conflicts

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,44 @@
 // src/App.tsx
-import React from 'react';
+import React from "react";
 import {
   BrowserRouter as Router,
   Routes,
-  Route
-} from 'react-router-dom';
-import DashboardContainer from './views/DashboardContainer';
-import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute';
-import LandingPage from './components/LandingPage/LandingPage'
+  Route,
+  useNavigate,
+} from "react-router-dom";
+import DashboardContainer from "./views/DashboardContainer";
+import LoginPage from "./components/LoginPage/LoginPage";
+import { OktaAuth, toRelativeUrl } from "@okta/okta-auth-js";
+import { Security } from "@okta/okta-react";
 
 const App: React.FC = () => {
   const isAuthenticated = true;
 
+  const navigate = useNavigate();
+  const restoreOriginalUri = (_oktaAuth, originalUri) => {
+    navigate(toRelativeUrl(originalUri || "/", window.location.origin));
+  };
+
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<LandingPage/>}></Route>
-        <Route 
-          path="/user" 
-          element={
-            <ProtectedRoute isAuthenticated={isAuthenticated} redirectTo="/">
-              <DashboardContainer />
-            </ProtectedRoute>
-          }
-        />
-      </Routes>
+      <Security oktaAuth={oktaAuth} restoreOriginalUri={restoreOriginalUri}>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute
+                isAuthenticated={isAuthenticated}
+                redirectTo="/login"
+              >
+                <DashboardContainer />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </Security>
     </Router>
   );
-}
+};
 
 export default App;
--- src/components/ProtectedRoute/ProtectedRoute.tsx
+++ src/components/ProtectedRoute/ProtectedRoute.tsx
@@ -1,18 +1,39 @@
-import React, { ReactElement } from 'react';
-import { Navigate } from 'react-router-dom';
+/*
+ * Copyright (c) 2021-Present, Okta, Inc. and/or its affiliates. All rights reserved.
+ * The Okta software accompanied by this notice is provided pursuant to the Apache License, Version 2.0 (the "License.")
+ *
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the License for the specific language governing permissions and limitations under the License.
+ */
 
-interface ProtectedRouteProps {
-  isAuthenticated: boolean;
-  redirectTo: string;
-  children: ReactElement;
+import React, { useEffect } from 'react';
+import { useOktaAuth } from '@okta/okta-react';
+import { toRelativeUrl } from '@okta/okta-auth-js';
+import { Outlet } from 'react-router-dom';
+import Loading from './Loading';
+
+export const RequiredAuth = () => {
+  const { oktaAuth, authState } = useOktaAuth();
+
+  useEffect(() => {
+    if (!authState) {
+      return;
+    }
+
+    if (!authState?.isAuthenticated) {
+      const originalUri = toRelativeUrl(window.location.href, window.location.origin);
+      oktaAuth.setOriginalUri(originalUri);
+      oktaAuth.signInWithRedirect();
+    }
+  }, [oktaAuth, !!authState, authState?.isAuthenticated]);
+
+  if (!authState || !authState?.isAuthenticated) {
+    return (<Loading />);
+  }
+
+  return (<Outlet />);
 }
-
-const ProtectedRoute: React.FC<ProtectedRouteProps> = ({
-  isAuthenticated,
-  redirectTo,
-  children,
-}): ReactElement => {
-  return isAuthenticated ? children : <Navigate to={redirectTo} replace />;
-};
-
-export default ProtectedRoute;
