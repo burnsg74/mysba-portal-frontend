b1b3159a7751c85a32db8845f15196ab0e706c8f
Gregory D. Burns <greg@burnswebservices.com>
2024-01-10 09:08:37 -0800

    MAT-737 Added Okta Login and cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,50 @@
-// src/App.tsx
 import * as React from 'react';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import DashboardContainer from './views/DashboardContainer';
-import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute';
 import LandingPage from './components/LandingPage/LandingPage'
+import {useNavigate} from 'react-router-dom';
+import {OktaAuth} from '@okta/okta-auth-js';
+import {Security} from '@okta/okta-react';
+import {LoginCallback} from '@okta/okta-react';
+import Loading from "./components/Loading";
+import ProtectedRoute from "./components/ProtectedRoute/ProtectedRoute";
+import ProfileContainer from "./views/ProfileContainer";
+import BusinessesContainer from "./views/BusinessesContainer";
+import CertificationsContainer from "./views/CertificationsContainer";
+import LoansContainer from "./views/LoansContainer";
+import HelpContainer from "./views/HelpContainer";
 
 const App: React.FC = () => {
-  const isAuthenticated = true;
+    const navigate = useNavigate();
+    const oktaAuth = new OktaAuth({
+        clientId: '0oacsfgduKvV9LKa80j6',
+        issuer: 'https://sbadev.okta-gov.com/oauth2/default',
+        redirectUri: `${window.location.origin}/login/callback`,
+        postLogoutRedirectUri: `${window.location.origin}`,
+        scopes: ['openid', 'profile', 'email'],
+        pkce: true
+    });
+    console.log("oktaAuth: ", oktaAuth)
+    const restoreOriginalUri = () => {
+        navigate('/dashboard')
+    };
 
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LandingPage/>}></Route>
-        <Route 
-          path="/user" 
-          element={
-            <ProtectedRoute isAuthenticated={isAuthenticated} redirectTo="/">
-              <DashboardContainer />
-            </ProtectedRoute>
-          }
-        />
-      </Routes>
-    </Router>
-  );
+    return (
+        <Security oktaAuth={oktaAuth} restoreOriginalUri={restoreOriginalUri}>
+            <Routes>
+                <Route path="/" element={<LandingPage/>}></Route>
+                <Route path="/login/callback" element={<LoginCallback loadingElement={<Loading/>}/>}/>
+                <Route element={<ProtectedRoute/>}>
+                    <Route path="/dashboard" element={<DashboardContainer/>}/>
+                    <Route path="/profile" element={<ProfileContainer/>}/>
+                    <Route path="/businesses" element={<BusinessesContainer/>}/>
+                    <Route path="/certifications" element={<CertificationsContainer/>}/>
+                    <Route path="/loans" element={<LoansContainer/>}/>
+                    <Route path="/help" element={<HelpContainer/>}/>
+                </Route>
+            </Routes>
+        </Security>
+    );
 }
 
 export default App;
--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -1,79 +1,46 @@
 import React from "react";
 import SBALogo from "../../assets/SBA-Logo-Horizontal.png";
+import { useEffect, useState } from 'react';
+import { useOktaAuth } from '@okta/okta-react';
+import { useNavigate } from "react-router-dom";
+
 
 const LandingPage = () => {
-  return (
-    <div>
-      <header className="bg-gray-800 text-white text-center p-4 flex justify-between items-center">
-        <a href="/" title="Home" aria-label="Home">
-          <img src={SBALogo} alt="Sweet Delights" className="h-8" />
-        </a>
-        <button className="text-blue-700 bg-blue-100 hover:bg-blue-200 font-semibold py-2 px-4 border border-blue-500 rounded-full mr-5">
-          Sign In
-        </button>
-      </header>
+    const { authState, oktaAuth } = useOktaAuth();
+    const navigate = useNavigate();
 
-      <section className="hero bg-blue-100 p-8 text-center">
-        <h2 className="text-2xl font-bold">Welcome to MyApp</h2>
-        <p className="mt-4 text-gray-700">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur nec
-          diam nec lorem placerat elementum.
-        </p>
-        <button className="mt-4 bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600">
-          Get Started
-        </button>
-      </section>
+    useEffect(() => {
+        if (!authState || !authState.isAuthenticated) {
+            // setUserInfo(null);
+            console.log("Not authenticated")
+        } else {
+            console.log("Authenticated")
+            oktaAuth.getUser().then((info) => {
+                // setUserInfo(info);
+                // navigate('/dashboard');
+                console.log("User info: ", info)
+            });
+        }
+    }, [oktaAuth, authState]);
 
-      <section id="features" className="p-8">
-        <h2 className="text-xl font-bold">Features</h2>
-        <ul className="list-disc pl-5 mt-4">
-          <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
-          <li>Phasellus vel ligula sit amet nulla iaculis aliquet.</li>
-          <li>
-            Fusce efficitur, eros sed suscipit porttitor, urna arcu hendrerit
-            libero.
-          </li>
-        </ul>
-      </section>
+    const login = async () => {
+        console.log("Login")
+        await oktaAuth.signInWithRedirect();
+        console.log("Login After")
+    };
 
-      <section id="about" className="bg-gray-50 p-8">
-        <h2 className="text-xl font-bold">About Us</h2>
-        <p className="mt-4 text-gray-700">
-          Praesent imperdiet enim a nisi commodo, eget accumsan arcu
-          scelerisque. Nulla vitae convallis nunc.
-        </p>
-      </section>
-
-      <section id="contact" className="p-8">
-        <h2 className="text-xl font-bold">Contact Us</h2>
-        <form className="mt-4 flex flex-col items-center">
-          <input
-            type="text"
-            placeholder="Name"
-            className="mt-2 p-2 border rounded"
-          />
-          <input
-            type="email"
-            placeholder="Email"
-            className="mt-2 p-2 border rounded"
-          />
-          <textarea
-            placeholder="Your message"
-            className="mt-2 p-2 border rounded"
-          ></textarea>
-          <button
-            type="submit"
-            className="mt-4 bg-green-500 text-white py-2 px-4 rounded hover:bg-green-600"
-          >
-            Send Message
-          </button>
-        </form>
-      </section>
-      <footer className="bg-gray-800 text-white text-center p-4">
-        <p>MyApp © 2024. All rights reserved.</p>
-      </footer>
-    </div>
-  );
+    return (
+        <div>
+            <header className="flex justify-between items-center m-3 border-b pb-3">
+                <img src={SBALogo} alt="Sweet Delights" className="h-8"/>
+                <button
+                    onClick={login}
+                    className="text-blue-700 bg-blue-100 hover:bg-blue-200 font-semibold py-2 px-4 border border-blue-500 rounded-full mr-5 float-right">
+                    Sign In
+                </button>
+            </header>
+        </div>
+    );
 };
 
 export default LandingPage;
