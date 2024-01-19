894fc3f33e16ca79b76fc56797b5c9d33afaa35a
Gregory D. Burns <greg@burnswebservices.com>
2024-01-18 16:23:15 -0800

    MAT-720 Dashboard with mock data

--- src/pages/Businesses/Businesses.tsx
+++ src/pages/Businesses/Businesses.tsx
@@ -1,11 +1,57 @@
 import React from 'react';
+import {useSelector} from "react-redux";
+import {getUser} from "src/store/user/userSlice";
+import {Link} from "react-router-dom";
+import CardLearningCenter from "src/components/CardLearningCenter/CardLearningCenter";
+import styles from "src/pages/Businesses/Businesses.module.css";
+
 
 const Businesses = () => {
+    const user: IUser = useSelector(getUser);
     return (
-        <>
-            <h1>Businesses</h1>
-        </>
-    );
+        <div className="grid-row">
+            <div className={`grid-col ${styles['container']}`}>
+                <h1 className={`${styles['title']}`}>Your Business</h1>
+                <div className="Businesses-content">
+                    {user.businesses && user.businesses.map((business, index) => (
+                        <React.Fragment key={index}>
+                            <div className="usa-card__container">
+                                <div className={`usa-card__header ${styles['card__header']}`}>
+                                    <div className="grid-row">
+                                        <div className="grid-col-auto">
+                                            <svg className="usa-icon text-middle pe-10" aria-hidden="true"
+                                                 focusable="false"
+                                                 role="img">
+                                                <use
+                                                    xlinkHref="/node_modules/@uswds/uswds/dist/img/sprite.svg#store"></use>
+                                            </svg>
+                                        </div>
+                                        <div className="grid-col">
+                                            <h2 className="usa-card__heading sba-blue text-middle">{business.name} </h2>
+                                        </div>
+                                        <div className="grid-col-auto">
+                                        <button type="button" className="usa-button">Details</button>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="usa-card__body">
+                                    <div className="grid-row sba-blue">
+                                        <div className="grid-col text-center">{business.type}</div>
+                                        <div className="grid-col text-center">UEI: {business.uei}</div>
+                                        <div className="grid-col text-center">EIN: {business.ein}</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </React.Fragment>
+                    ))}
+                </div>
+            </div>
+            <div className="grid-col-auto">
+                <CardLearningCenter/>
+            </div>
+        </div>
+    )
+        ;
 };
 
 export default Businesses;
--- src/pages/Loans/Loans.tsx
+++ src/pages/Loans/Loans.tsx
@@ -1,11 +1,53 @@
 import React from 'react';
+import CardLearningCenter from "src/components/CardLearningCenter/CardLearningCenter";
+import styles from "src/pages/Loans/Loans.module.css";
+import Gears from "src/assets/loans-gears.svg";
 
 const Loans = () => {
     return (
         <>
-            <h1>Loans</h1>
+            <div className="grid-row">
+                <div className="grid-col">
+                    <div className="grid-row">
+                        <div className="grid-col">
+                            <img src={Gears} className={`${styles["title__img"]}`}/>
+                        </div>
+                    </div>
+                    <div className="grid-row">
+                        <div className="grid-col">
+                            <div className={`${styles["title"]}`}>Under Construction</div>
+                        </div>
+                    </div>
+                    <div className="grid-row">
+                        <div className="grid-col">
+                            <div className={`sba-blue ${styles["title__text"]}`}>Linking your loans is currently under construction.
+                                Check back in our next release for loan functionality.
+                                To check on your loans please visit your <a href="https://lending.sba.gov" target="_blank">loan portal</a>.
+                            </div>
+                        </div>
+                    </div>
+                    <div className="grid-row">
+                        <div className="grid-col">
+                            <div className={`${styles["title__btn"]}`}>
+                                <button
+                                    type="button"
+                                    className="usa-button"
+                                    onClick={(e) => {
+                                        e.preventDefault();
+                                        window.open('https://lending.sba.gov', '_blank');
+                                    }}
+                                >View the Loan Portal
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="grid-col-auto">
+                    <CardLearningCenter/>
+                </div>
+            </div>
         </>
-    );
+    )
 };
 
 export default Loans;
