cd9a773091b1ec1a15610381982d8101809e4afa
Gregory D. Burns <greg@burnswebservices.com>
2024-02-08 18:21:26 -0800

    MAT-955 : Certification - Detail View with Mock Data - Update styling and responsiveness for a non-expired certification. - Did some cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,58 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import {useNavigate} from 'react-router-dom';
-import {OktaAuth} from '@okta/okta-auth-js';
-import {Security} from '@okta/okta-react';
-import {LoginCallback} from '@okta/okta-react';
+import React from "react";
+import { Route, Routes, useNavigate } from "react-router-dom";
+import { OktaAuth } from "@okta/okta-auth-js";
+import { LoginCallback, Security } from "@okta/okta-react";
 import Businesses from "src/pages/Businesses/Businesses";
 import Certifications from "src/pages/Certifications/Certifications";
-import Dashboard from 'src/pages/Dashboard/Dashboard';
+import Dashboard from "src/pages/Dashboard/Dashboard";
 import Help from "src/pages/Help/Help";
-import Landing from 'src/pages/Landing/Landing'
+import Landing from "src/pages/Landing/Landing";
 import Loading from "src/pages/Loading/Loading";
 import Loans from "src/pages/Loans/Loans";
 import Profile from "src/pages/Profile/Profile";
 import ProtectedRoute from "./components/ProtectedRoute/ProtectedRoute";
-import CertificationView from "src/pages/CertificationView/CertificationView";
+import CertificationDetail from "src/pages/CertificationDetail/CertificationDetail";
 import AccountSetup1 from "src/pages/AccountSetup1/AccountSetup1";
 import AccountSetup2 from "src/pages/AccountSetup2/AccountSetup2";
 
 // @TODO Move this to a config file
 const oktaAuth = new OktaAuth({
-    clientId: '0oacsfgduKvV9LKa80j6',
-    issuer: 'https://sbadev.okta-gov.com/oauth2/default',
-    redirectUri: `${window.location.origin}/login/callback`,
-    postLogoutRedirectUri: `${window.location.origin}`,
-    scopes: ['openid', 'profile', 'email'],
-    pkce: true
+  clientId: "0oacsfgduKvV9LKa80j6",
+  issuer: "https://sbadev.okta-gov.com/oauth2/default",
+  redirectUri: `${window.location.origin}/login/callback`,
+  postLogoutRedirectUri: `${window.location.origin}`,
+  scopes: ["openid", "profile", "email"],
+  pkce: true,
 });
 
 const App: React.FC = () => {
-    const navigate = useNavigate();
-    const restoreOriginalUri = () => {
-        navigate('/')
-    };
+  const navigate = useNavigate();
+  const restoreOriginalUri = () => {
+    navigate("/");
+  };
 
-    return (
-        <Security oktaAuth={oktaAuth} restoreOriginalUri={restoreOriginalUri}>
-            <Routes>
-                <Route path="/" element={<Landing />}></Route>
-                <Route path="/login/callback" element={<LoginCallback loadingElement={<Loading/>}/>}/>
-                <Route element={<ProtectedRoute/>}>
-                    <Route path="/account-setup/1" element={<AccountSetup1/>}/>
-                    <Route path="/account-setup/2" element={<AccountSetup2/>}/>
-                    <Route path="/dashboard/*" element={<Dashboard/>}/>
-                    <Route path="/profile" element={<Profile/>}/>
-                    <Route path="/businesses" element={<Businesses/>}/>
-                    <Route path="/certifications" element={<Certifications/>}/>
-                    <Route path="/certification/:id" element={<CertificationView/>}/>
-                    <Route path="/loans" element={<Loans/>}/>
-                    <Route path="/help" element={<Help/>}/>
-                </Route>
-            </Routes>
-        </Security>
-    );
-}
+  return (
+    <Security oktaAuth={oktaAuth} restoreOriginalUri={restoreOriginalUri}>
+      <Routes>
+        <Route path="/" element={<Landing />}></Route>
+        <Route
+          path="/login/callback"
+          element={<LoginCallback loadingElement={<Loading />} />}
+        />
+        <Route element={<ProtectedRoute />}>
+          <Route path="/account-setup/1" element={<AccountSetup1 />} />
+          <Route path="/account-setup/2" element={<AccountSetup2 />} />
+          <Route path="/dashboard/*" element={<Dashboard />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/businesses" element={<Businesses />} />
+          <Route path="/certifications" element={<Certifications />} />
+          <Route path="/certification/:id" element={<CertificationDetail />} />
+          <Route path="/loans" element={<Loans />} />
+          <Route path="/help" element={<Help />} />
+        </Route>
+      </Routes>
+    </Security>
+  );
+};
 
 export default App;
--- src/components/Field/Field.tsx
+++ src/components/Field/Field.tsx
@@ -1,22 +1,18 @@
-import React from 'react';
-import styles from 'src/components/Field/Field.module.css';
+import React from "react";
+import styles from "src/components/Field/Field.module.css";
 
 interface Props {
-    label: string;
-    value: string;
+  label: string;
+  value: string;
 }
 
-const Field: React.FC<Props> = ({label, value}) => {
-    return (
-        <div className={`${styles['row']}`}>
-            <div className={`${styles['label']}`}>
-                {label}
-            </div>
-            <div className={`${styles['value']}`}>
-                {value}
-            </div>
-        </div>
-    );
-}
+const Field: React.FC<Props> = ({ label, value }) => {
+  return (
+    <div className={`${styles["row"]}`}>
+      <div className={`${styles["label"]}`}>{label}</div>
+      <div className={`${styles["value"]}`}>{value}</div>
+    </div>
+  );
+};
 
 export default Field;
