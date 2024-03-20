24cf432eb9769a79480c0483e30305b55aac8fad
Gregory D. Burns <greg@burnswebservices.com>
2024-03-20 07:18:55 -0700

    MAT-1466 : Certifications Page - "Need help..." refinement.

--- src/pages/CertificationDetail/CertificationDetail.tsx
+++ src/pages/CertificationDetail/CertificationDetail.tsx
@@ -91,9 +91,12 @@ const CertificationDetail = () => {
         {certification.days_until_expiry <= 0 ? (
           <Alert
             type={"error"}
-            message={t("Your {{certification_type}} certification has expired", {
-              certification_type: t(certification.certification_type),
-            })}
+            message={t(
+              "Your {{certification_type}} certification has expired",
+              {
+                certification_type: t(certification.certification_type),
+              }
+            )}
           />
         ) : certification.days_until_expiry <= 90 ? (
           <Alert
@@ -154,80 +157,87 @@ const CertificationDetail = () => {
 
         {/* Expired Cert Help*/}
         {certification.days_until_expiry <= 0 && (
-          <>
-            <div className={`${styles["expired-help__container"]}`}>
-              <div className={`${styles["expired-help__header"]}`}>
-                {t("Need help getting re-certified?")}
+        <>
+          <div className={`${styles["expired-help__container"]}`}>
+            <div className={`${styles["expired-help__header"]}`}>
+              {t("Need help getting re-certified?")}
+            </div>
+            <div
+              className={`grid-row ${styles["expired-help__message__container"]}`}
+            >
+              <div className={`grid-col-auto`}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className={`usa-icon ${styles["expired-help__icon"]}`}
+                  viewBox="0 0 18 18"
+                  fill="none"
+                  aria-hidden="true"
+                  focusable="false"
+                  role="img"
+                >
+                  <path
+                    d="M16.95 18C14.8667 18 12.8083 17.5458 10.775 16.6375C8.74167 15.7292 6.89167 14.4417 5.225 12.775C3.55833 11.1083 2.27083 9.25833 1.3625 7.225C0.454167 5.19167 0 3.13333 0 1.05C0 0.75 0.1 0.5 0.3 0.3C0.5 0.1 0.75 0 1.05 0H5.1C5.33333 0 5.54167 0.0791667 5.725 0.2375C5.90833 0.395833 6.01667 0.583333 6.05 0.8L6.7 4.3C6.73333 4.56667 6.725 4.79167 6.675 4.975C6.625 5.15833 6.53333 5.31667 6.4 5.45L3.975 7.9C4.30833 8.51667 4.70417 9.1125 5.1625 9.6875C5.62083 10.2625 6.125 10.8167 6.675 11.35C7.19167 11.8667 7.73333 12.3458 8.3 12.7875C8.86667 13.2292 9.46667 13.6333 10.1 14L12.45 11.65C12.6 11.5 12.7958 11.3875 13.0375 11.3125C13.2792 11.2375 13.5167 11.2167 13.75 11.25L17.2 11.95C17.4333 12.0167 17.625 12.1375 17.775 12.3125C17.925 12.4875 18 12.6833 18 12.9V16.95C18 17.25 17.9 17.5 17.7 17.7C17.5 17.9 17.25 18 16.95 18ZM3.025 6L4.675 4.35L4.25 2H2.025C2.10833 2.68333 2.225 3.35833 2.375 4.025C2.525 4.69167 2.74167 5.35 3.025 6ZM11.975 14.95C12.625 15.2333 13.2875 15.4583 13.9625 15.625C14.6375 15.7917 15.3167 15.9 16 15.95V13.75L13.65 13.275L11.975 14.95Z"
+                    fill="#002E6D"
+                  />
+                </svg>
               </div>
               <div
-                className={`grid-row ${styles["expired-help__message__container"]}`}
+                className={`grid-col ${styles["expired-help-message__container"]}`}
               >
-                <div className={`grid-col-auto`}>
+                <div className={`${styles["expired-help__message__header"]}`}>
+                  {t("Contact Us")}
+                </div>
+                <div className={`${styles["expired-help__message__body"]}`}>
+                  {t("You can fill out a help request form")}&nbsp;
+                  <u>
+                    <a
+                      href="https://wosb.certify.sba.gov/help-csh/"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      {t("here")}
+                    </a>
+                  </u>
+                  &nbsp;
+                  {t("or contact us at")}&nbsp;
+                  <u>
+                    <a href="mailto:wosb@SBA.gov">WOSB@SBA.gov</a>
+                  </u>
+                  .
+                </div>
+                <div className={`${styles["expired-help__message__body"]}`}>
+                  <a
+                    href="https://wosb.certify.sba.gov/knowledgebase/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    style={{ display: "flex", alignItems: "center" }}
+                  >
+                    {t("Frequently Asked Questions")}
+                  </a>
                   <svg
-                    className={`usa-icon ${styles["expired-help__icon"]}`}
+                    className={`usa-icon ${styles["expired-help__launch-icon"]}`}
                     aria-hidden="true"
                     focusable="false"
                     role="img"
+                    width={24}
+                    height={24}
                   >
-                    <use xlinkHref="/assets/img/sprite.svg#phone"></use>
+                    <use xlinkHref="/assets/img/sprite.svg#launch"></use>
                   </svg>
-                </div>
-                <div
-                  className={`grid-col ${styles["expired-help-message__container"]}`}
-                >
-                  <div className={`${styles["expired-help__message__header"]}`}>
-                    {t("Contact Us")}
-                  </div>
-                  <div className={`${styles["expired-help__message__body"]}`}>
-                    {t("You can fill out a help request form")}{" "}
-                    <u>
-                      <a
-                        href="https://wosb.certify.sba.gov/help-csh/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        {t("here")}
-                      </a>
-                    </u>{" "}
-                    {t("or contact us at")}{" "}
-                    <u>
-                      <a href="mailto:wosb@SBA.gov">WOSB@SBA.gov</a>
-                    </u>
-                    .
-                  </div>
-                  <div className={`${styles["expired-help__message__body"]}`}>
-                    <a
-                      href="https://wosb.certify.sba.gov/knowledgebase/"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      style={{ display: "flex", alignItems: "center" }}
-                    >
-                      {t("Frequently Asked Questions")}
-                    </a>
-                      <svg
-                        className="usa-icon"
-                        aria-hidden="true"
-                        focusable="false"
-                        role="img"
-                        width={24}
-                        height={24}
-                      >
-                        <use xlinkHref="/assets/img/sprite.svg#launch"></use>
-                      </svg>
-                  </div>
                 </div>
               </div>
             </div>
-            <button
-              type="button"
-              className={`usa-button usa-button--outline ${styles["start-recertification__btn"]}`}
-              onClick={() =>
-                window.open("https://wosb.certify.sba.gov/", "_blank")
-              }
-            >
-              {t("Start the re-certification process")}
-            </button>
-          </>
+          </div>
+          <button
+            type="button"
+            className={`usa-button usa-button--outline ${styles["start-recertification__btn"]}`}
+            onClick={() =>
+              window.open("https://wosb.certify.sba.gov/", "_blank")
+            }
+          >
+            {t("Start the re-certification process")}
+          </button>
+        </>
         )}
         <div className={`${styles["subtitle"]}`}>{t("Details")}</div>
         <Field
@@ -235,10 +245,7 @@ const CertificationDetail = () => {
           value={certification.company_name ?? ""}
         />
         <Field label="Issue Date" value={issue_date ?? ""} />
-        <Field
-          label="Expiration Date"
-          value={expiration_date ?? ""}
-        />
+        <Field label="Expiration Date" value={expiration_date ?? ""} />
         {/*@TODO : Missing naics from data layer*/}
         {/*<Field*/}
         {/*  label="North American Industry Classification System"*/}
