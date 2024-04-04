8e68bd984121d69451d035ce98bc4408e974e32f
Gregory D. Burns <greg@burnswebservices.com>
2024-04-04 14:05:33 -0700

    Add .env.localhost file and sprite.svg
    A new file, .env.localhost, has been introduced, containing environment variables for local development - including VITE_APP_BASE_API_URL and VITE_APP_OKTA properties. In addition, a new image file 'sprite.svg' has been added to the public assets directory.

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -16,16 +16,13 @@ const Dashboard = () => {
   const location = useLocation();
   const { t } = useTranslation();
 
-  return (
-    <>
+  return (<>
       {/* Top city banner */}
       <div className={`banner ${styles["banner"]}`}>
         <div className={`${styles["welcome-message"]}`}>
-          {user.profile?.crm && (
-            <span className="username">
+          {user.profile?.crm && (<span className="username">
               {t("Hi")} {user.profile.crm.first_name},{" "}
-            </span>
-          )}
+            </span>)}
         </div>
         <div className={`${styles["mysba-message"]}`}>{t("Welcome to your MySBA Dashboard")}</div>
         <img className={`${styles["cityscape"]}`} src={CityScapeImage} alt={t("Decorative Cityscape")} />
@@ -35,44 +32,29 @@ const Dashboard = () => {
       <div className={`main-container`}>
         <div className={`${styles["dashboard-content"]}`}>
           {/* Certifications Alerts */}
-          {user.certifications &&
-            user.certifications.map((certification, index) => {
-              const expiration_date = formatDate(certification.expiration_date, "MM/DD/YYYY");
-              return (
-                <React.Fragment key={index}>
-                  {certification.days_until_expiry <= 0 ? (
-                    <div className={`${styles["alert__container"]}`}>
-                      <Alert
-                        key={index}
-                        type={"error"}
-                        message={t("Your" + " " + certification.certification_type + " " + "certification has expired")}
-                      />
-                    </div>
-                  ) : certification.days_until_expiry <= 90 ? (
-                    <div className={`${styles["alert__container"]}`}>
-                      <Alert
-                        key={index}
-                        type="warning"
-                        message={
-                          t(
-                            "Your" + " " + certification.certification_type + " " + "certification must be renewed by"
-                          ) +
-                          " " +
-                          expiration_date
-                        }
-                      />
-                    </div>
-                  ) : null}
-                </React.Fragment>
-              );
-            })}
+          {user.certifications && user.certifications.map((certification, index) => {
+            const expiration_date = formatDate(certification.expiration_date, "MM/DD/YYYY");
+            return (<React.Fragment key={index}>
+                {certification.days_until_expiry <= 0 ? (<div className={`${styles["alert__container"]}`}>
+                    <Alert
+                      key={index}
+                      type={"error"}
+                      message={t("Your" + " " + certification.certification_type + " " + "certification has expired")}
+                    />
+                  </div>) : certification.days_until_expiry <= 90 ? (<div className={`${styles["alert__container"]}`}>
+                    <Alert
+                      key={index}
+                      type="warning"
+                      message={t("Your" + " " + certification.certification_type + " " + "certification must be renewed by") + " " + expiration_date}
+                    />
+                  </div>) : null}
+              </React.Fragment>);
+          })}
 
           {/* Businesses */}
           <div className={`${styles.businessesContainer}`}>
-            {user.businesses &&
-              [...user.businesses].sort((a, b) => a.name.localeCompare(b.name))
-                .map((business, index) => (
-              <React.Fragment key={index}>
+            {user.businesses && [...user.businesses].sort((a, b) => a.name.localeCompare(b.name))
+              .map((business, index) => (<React.Fragment key={index}>
                 <div className={`${styles.businessContainer}`}>
                   <div className={`grid-row ${styles.businessHeaderRow}`}>
                     <div className={`grid-col-auto`}>
@@ -95,25 +77,20 @@ const Dashboard = () => {
                       </Link>
                     </div>
                   </div>
-                  <div className="grid-row">
-                    <div className="grid-col">
-                      {user.certifications &&
-                        user.certifications
-                          .filter(certification => certification.business_id === business.id)
-                          .map((certification, index) => (
-                            <CertificationCard key={index} index={index + 1} certification={certification} />
-                          ))}
-                    </div>
-                  </div>
+                  {user.certifications && user.certifications
+                    .filter(certification => certification.business_id === business.id)
+                    .map((certification, index) => (<div key={index} className={`grid-row ${styles.certificationsRow}`}>
+                      <div className="grid-col">
+                        <CertificationCard index={index + 1} certification={certification} />
+                      </div>
+                    </div>))}
                 </div>
-              </React.Fragment>
-            ))}
+              </React.Fragment>))}
           </div>
         </div>
       </div>
       <AccountSetupModal showModal={location.pathname === "/dashboard/new"} />
-    </>
-  );
+    </>);
 };
 
 export default Dashboard;
