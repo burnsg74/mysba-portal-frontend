514ab325a97ce3945d09cd2f119e1631a250c82a
Peter Cofrancesco <pcofrancesco@makpar.com>
2024-06-03 15:27:08 -0400

    MAT-1982 CX Review Changes

--- src/components/LinkCertModalGroup/LinkCertModal1.tsx
+++ src/components/LinkCertModalGroup/LinkCertModal1.tsx
@@ -1,32 +1,88 @@
 import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
+import { useOktaAuth } from "@okta/okta-react";
 import Modal from "src/components/Modal/Modal";
 import styles from "src/components/LinkCertModalGroup/LinkCertModal.module.css";
 import modalIcon from "src/assets/MySBAIllustrationsIcons.svg";
+import axios from "axios";
+import { useSelector } from "react-redux";
+import { getUser } from "src/store/user/userSlice";
+import { AccessToken } from "@okta/okta-auth-js";
+import { BASE_API_URL } from "src/utils/constants";
 
 interface Step1ModalProps {
     businessData: {
-        name: string
-        id: string
+        businessName: string
+        certName: string
         uei: string
     };
     handleClose: () => void;
     handleContinue: (stepData: any) => void;
 }
 
+type data = {
+    name: string
+    business_address_city: string
+    business_address_street1: string
+    business_address_street2: string
+    business_address_state: string
+    business_address_zip: string
+    county: string
+    ein: string
+    id: string
+    legal_entity: string
+    owner: string
+    uei: string
+    workingWithSBA: string
+  };
+
 const LinkCertModal1: React.FC<Step1ModalProps> = ({ businessData, handleClose, handleContinue }) => {
     const { t } = useTranslation();
-    const [stepData, setStepData] = useState<{ uei: string; }>({ uei: "" });
+    const user: IUser = useSelector(getUser);
+    const [stepData, setStepData] = useState<{ uei: string, businessName:string, certName:string; }>({ uei: "", businessName: "", certName:"" });
+    const [error, setError] = useState<string | null>(null);
+    const { authState } = useOktaAuth();
+    
+    const linkNewCert = async (data:data) => {
+        console.log("linkNewCert", data);
+    
+        try {
+          const email = user?.profile?.crm?.email;
+          if (!email) {
+            throw new Error("Email is not defined");
+          }
+          let accessToken: string | AccessToken | null | undefined;
+    
+          if (authState && "accessToken" in authState) {
+            accessToken = authState.accessToken?.accessToken;
+          } else {
+            accessToken = undefined;
+          }
+          data.id=email;
+          //   axios.defaults.headers.common["Authorization"] = `Bearer ${accessToken}`;
+          //   const res = await axios.post(`${BASE_API_URL}business`, data);
+          console.log("saveNewBusinesses res");
+        } catch (error) {
+          console.error("Link New Cert Error", error);
+          setError("UEI could not be linked to an existing business.");
+        }
+      }
 
     const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         const value = event.target.value;
-        const updatedStepData = { ...stepData, uei: value };
+        const updatedStepData = { ...stepData, uei: value, businessName:"Mock Business", certName:"Mock Cert Title" };
         setStepData(updatedStepData);
     };
 
-    function handleContinueBtnClick() {
-        handleContinue(stepData);
-    }
+    const handleContinueBtnClick = () => {
+        console.log(stepData)
+        if (stepData.uei.length === 12) {
+            setError(null);
+            handleContinue(stepData);
+        } else {
+            setError("UEI must be 12 characters");
+        }
+    };
 
     const closeModal = () => {
         handleClose();
@@ -61,6 +117,7 @@ const LinkCertModal1: React.FC<Step1ModalProps> = ({ businessData, handleClose, 
                 </span>
                 <br />
                 <span className={`${styles.greyLabel}`}>Unique Entity Identifier (12 Characters)</span>
+                {error && <span className={styles.error}>{error}</span>}
             </label>
             <input className={`usa-input ${styles.textInput}`} id="input-type-text" name="input-type-text" onChange={handleInputChange} />
         </div>
--- src/components/LinkCertModalGroup/LinkCertModal2.tsx
+++ src/components/LinkCertModalGroup/LinkCertModal2.tsx
@@ -5,17 +5,17 @@ import styles from "src/components/LinkCertModalGroup/LinkCertModal.module.css";
 import modalIcon from "src/assets/bag.svg";
 
 interface Step2ModalProps {
-    businessData: { name: string
-        id: string
+    businessData: { businessName: string
+        certName: string
         uei: string };
     handleClose: () => void;
     handleContinue: (stepData: any) => void;
     handleBack: (stepData: any) => void;
 }
 
-const LinkCertModal2: React.FC<Step2ModalProps> = ({ businessData, handleClose, handleContinue }) => {
+const LinkCertModal2: React.FC<Step2ModalProps> = ({ businessData, handleClose, handleContinue, handleBack }) => {
     const { t } = useTranslation();
-    const [stepData, setStepData] = useState<{ businessData: string; }>({ businessData: businessData.name });
+    const [stepData, setStepData] = useState<{ uei:string, businessName: string, certName:string ; }>({  uei: businessData.uei, businessName: businessData.businessName,certName: businessData.certName });
 
     function handleContinueBtnClick() {
         handleContinue(stepData);
@@ -24,6 +24,9 @@ const LinkCertModal2: React.FC<Step2ModalProps> = ({ businessData, handleClose, 
     const closeModal = () => {
         handleClose();
     };
+    const handleBackButtonClick = () => {
+        handleBack(stepData);
+    };
 
     return (<Modal
         title={t("Link a Certification")}
@@ -31,13 +34,13 @@ const LinkCertModal2: React.FC<Step2ModalProps> = ({ businessData, handleClose, 
         totalSteps={3}
         completedSteps={1}
         ImageAndAlt={{ image: modalIcon, alt: "Modal Icon" }}
-        contentTitle={t("Is [name of business] your business?")}
+        contentTitle={t(`Is ${businessData.businessName} your business?`)}
         contentMessage={t("If this business name doesn't match, please go back to the previous step and make sure you entered your UEI correctly.")}
         footerContent={(<>
             <button
                 type="button"
                 className={`usa-button usa-button--outline ${styles.cancelBtn}`}
-                onClick={closeModal}
+                onClick={handleBackButtonClick}
             >
                 {t("Back")}
             </button>
