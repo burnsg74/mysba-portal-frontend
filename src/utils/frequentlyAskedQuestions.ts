464f91788c8a17dd6f3db1b3dc328d6910f4ce33
greg <greg@burnswebservices.com>
2024-06-05 09:03:34 -0400

    Update FAQ content

--- src/utils/frequentlyAskedQuestions.ts
+++ src/utils/frequentlyAskedQuestions.ts
@@ -3,28 +3,52 @@ type FrequentlyAskedQuestion = {
 };
 const SUPPORT_EMAIL = "digitalresearch@SBA.gov";
 
-const FrequentlyAskedQuestions: Array<FrequentlyAskedQuestion> = [{
-  id: 1,
-  question: "What is MySBA?",
-  answer: `You will be able to provide feedback on this test environment and your respective experience via review sessions the SBA will schedule with you and other test users. You are welcome to share additional thoughts by emailing ${SUPPORT_EMAIL} as well.`},{
-  id: 2,
-  question: "How do I get help on features in this portal?",
-  answer: `This is a test environment and we are building out the help features. For assistance, email ${SUPPORT_EMAIL} and you will receive a reply within two business days.`},{
-  id: 3,
-  question: "How do I change my username?",
-  answer: `The ability to change your username will be available in future versions with directions available here. If you need more information, email ${SUPPORT_EMAIL} and you will receive a reply within two business days.` },{
-  id: 4,
-  question: "How do I change or get a new password?",
-  answer: `The ability to change your password or get a new one will be available in future versions with directions available here. If you need more information, email ${SUPPORT_EMAIL} and you will receive a reply within two business days.`},{
-  id: 5,
-  question: "Is my information saved somewhere?",
-  answer: `No information is saved in this test environment. Details about how your information will be saved will be available here in future versions. If you no longer want to be part of this test, email ${SUPPORT_EMAIL} and you will receive a reply within two business days.`},{
-  id: 6,
-  question: "How do I delete my account?",
-  answer: `The ability to delete your account will be available in future versions with directions available here. If you no longer want to be part of this test, email ${SUPPORT_EMAIL} and you will receive a reply within two business days.` },{
-  id: 7,
-  question: "What happens to my information if I delete my account?",
-  answer: `No information is saved in this test environment. Details about what happens to your information after an account is deleted will be available here in future versions. If you no longer want to be part of this test, email ${SUPPORT_EMAIL} and you will receive a reply within two business days.`
-}];
+const FrequentlyAskedQuestions: Array<FrequentlyAskedQuestion> = [
+  {
+    "id": 1,
+    "question": "What is MySBA?",
+    "answer": "MySBA is the central space to view your actions with the SBA on the path to building a resilient, successful business. It’s a virtual powerhouse that keeps all of your SBA products in one place to give you peace of mind and to remind you that the SBA is in your corner. Access your loans and certifications with a click of a button and propel your business forward with resources curated for you."
+  },
+  {
+    "id": 2,
+    "question": "How do I get help on features in MySBA?",
+    "answer": "This is a test environment and we are building out the help features. For assistance, email digitalresearch@sba.gov and you will receive a reply within two business days."
+  },
+  {
+    "id": 3,
+    "question": "How do I change my username?",
+    "answer": "The ability to change your username will be available in future versions with directions available here. If you need more information, email digitalresearch@sba.gov and you will receive a reply within two business days."
+  },
+  {
+    "id": 4,
+    "question": "How do I change or get a new password?",
+    "answer": "The ability to change your password or get a new one will be available in future versions with directions available here. If you need more information, email digitalresearch@sba.gov and you will receive a reply within two business days."
+  },
+  {
+    "id": 5,
+    "question": "Is my information saved somewhere?",
+    "answer": "No information is saved in this test environment. Details about how your information will be saved will be available here in future versions. If you no longer want to be part of this test, email digitalresearch@sba.gov and you will receive a reply within two business days."
+  },
+  {
+    "id": 6,
+    "question": "How do I delete my account?",
+    "answer": "The ability to delete your account will be available in future versions with directions available here. If you no longer want to be part of this test, email digitalresearch@sba.gov and you will receive a reply within two business days."
+  },
+  {
+    "id": 7,
+    "question": "What happens to my information if I delete my account?",
+    "answer": "No information is saved in this test environment. Details about what happens to your information after an account is deleted will be available here in future versions. If you no longer want to be part of this test, email digitalresearch@sba.gov and you will receive a reply within two business days."
+  },
+  {
+    "id": 8,
+    "question": "I don't have SBA loans or contracting certifications yet. Will I be able to connect them in MySBA when/if I do get them?",
+    "answer": "Yes. Via MySBA, you will be able to link to and connect additional SBA products and services, including new businesses, certifications, and loans, as you engage with them."
+  },
+  {
+    "id": 9,
+    "question": "I'm affected by a disaster. Am I able to find out if I’m eligible for a loan (and apply) through MySBA?",
+    "answer": "Confirming your eligibility for and applying for disaster assistance will be available in future versions with directions available here. If you need more information, email digitalresearch@sba.gov and you will receive a reply within two business days."
+  }
+];
 
 export { FrequentlyAskedQuestions };
