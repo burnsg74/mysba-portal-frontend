f807ed142e282e9b80f89bfb7b28771686d7e98c
greg <greg@burnswebservices.com>
2024-07-09 09:13:56 -0700

    Reactivate user profile navigation check

--- src/pages/Loading/Loading.tsx
+++ src/pages/Loading/Loading.tsx
@@ -99,15 +99,13 @@ const Loading = () => {
           dispatch(setNav(true));
           dispatch(setShowProfile(true))
           dispatch(setUser(user));
-          // Disable in RC1 (GB) 6/24/24
-          // if (!user.profile.portal) {
-          //   dispatch(setNav(false));
-          //   dispatch(setShowProfile(false))
-          //   navigate("/account-setup/1");
-          // } else {
-          //   navigate("/dashboard");
-          // }
-          navigate("/dashboard");
+          if (!user.profile.portal) {
+            dispatch(setNav(false));
+            dispatch(setShowProfile(false))
+            navigate("/account-setup/1");
+          } else {
+            navigate("/dashboard");
+          }
         }).catch((error) => {
           console.log('Catch Error 1', error);
           oktaAuth.signOut().then(() => {
