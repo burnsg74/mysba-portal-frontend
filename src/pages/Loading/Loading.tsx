8e126d3192e253f7e5857264d37bd50d3e2b55e7
greg <greg@burnswebservices.com>
2024-07-31 14:02:15 -0700

    No CRM Profile workaround

--- src/pages/Loading/Loading.tsx
+++ src/pages/Loading/Loading.tsx
@@ -55,7 +55,14 @@ const Loading = () => {
     });
     let individual = results?.data.individuals[0];
     if (!individual) {
-      throw new Error("No individual found");
+      // throw new Error("No individual found");
+
+      // Workaround for the case where the user is not in the CRM Profile yet. This is a temporary solution. - GB 2021-09-29
+      individual = {
+        firstName: info.given_name ?? "",
+        lastName: info.family_name ?? "",
+        email: info.email ?? "",
+      };
     }
     let crmData: IUserProfile["crm"] = {
       first_name: individual.firstName ?? "", last_name: individual.lastName ?? "", email: individual.email ?? "",
