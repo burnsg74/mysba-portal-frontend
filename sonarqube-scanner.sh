065e324c4a07d68d10a708a93c3dde45dd5ee2a4
greg <greg@burnswebservices.com>
2024-08-27 06:45:06 -0700

    MAT-2404: QA - Password Does not contain parts from username not working.

--- sonarqube-scanner.sh
+++ sonarqube-scanner.sh
@@ -1,5 +1,5 @@
 sonar-scanner \
-  -Dsonar.projectKey=mySBA-Portal \
+  -Dsonar.projectKey=MySBA \
   -Dsonar.sources=. \
   -Dsonar.host.url=http://localhost:9000 \
-  -Dsonar.token=sqp_ee045b93ac6583463aa240f9bb2c190012fc73ca
+  -Dsonar.token=${SONAR_SECRET}
